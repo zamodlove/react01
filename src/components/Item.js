fe5c1ea462caada53bedc6541d8ffe8256392d50
AntSky <34389473+zamodlove@users.noreply.github.com>
2022-06-20 08:56:20 +0700

    update div css v.02

---
+++ src/components/Item.js
@@ -0,0 +1,14 @@
+
+
+const Item = (props) => {
+  return (
+    <li>   {props.title}<span>{props.Amount}</span></li>
+    
+  )
+}
+
+export default Item
+
+
+
+
---
+++ src/components/Transaction.js
@@ -0,0 +1,31 @@
+
+import Item from './Item'
+import './transation.css'
+
+const Transaction = () => {
+  return (
+    
+    <ul className='item-list'>
+
+        <Item title= "ค่ารักษาพยายยบาล " Amount="20000" />
+        <Item title= "ค่าบัตรเครดิต   " Amount="7000" />
+        <Item title= "ค่าผ่อนรถ " Amount="1000" />
+        <Item title= "ค่าน้ำมัน " Amount="5000" />
+    
+
+    </ul>
+
+
+
+
+  )
+}
+
+export default Transaction
+
+
+
+
+
+
+
