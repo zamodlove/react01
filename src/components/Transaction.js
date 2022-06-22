e0cc3340506378d19b20f1b269d758fe89197097
AntSky <34389473+zamodlove@users.noreply.github.com>
2022-06-22 10:34:38 +0700

    upload v1.1

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -10,7 +10,7 @@ const Transaction = () => {
         <Item title= "ค่ารักษาพยายยบาล " Amount="20000" />
         <Item title= "ค่าบัตรเครดิต   " Amount="7000" />
         <Item title= "ค่าผ่อนรถ " Amount="1000" />
-        <Item title= "ค่าน้ำมัน " Amount="5000" />
+        <Item title= "ค่าน้ำมัน " Amount="50000" />
     
 
     </ul>
